005245bdd3abcbbfcc342bdeb2081fc94cf1df2c
Carl Hallén Jansson <callehj@hotmail.se>
2021-05-27 14:38:47 +0200

    Add landing page where you enter address

---
+++ src/pages/[address].tsx
@@ -0,0 +1,83 @@
+import { GetServerSideProps } from "next"
+
+import EthPrice from "../components/Data/EthPrice";
+import Unpaid from "../components/Data/Unpaid";
+import Wallet from "../components/Data/Wallet";
+import AverageHashrate from "../components/Data/AverageHashrate";
+import TotalHoldings from "../components/Data/TotalHoldings";
+import BinanceCandlestick from "../components/Data/BinanceCandlestick";
+import HashrateLineChart from "../components/Data/HashrateLineChart";
+
+import styles from '../styles/Address.module.css'
+import { MinerCurrentStats } from "../types/ethermine";
+import { BinanceAvgPrice, BinanceKline } from "../types/binance";
+import { ExchangeLatest } from "../types/exchangerate";
+
+interface Props{
+	ethermine: { data: MinerCurrentStats };
+	binance: BinanceAvgPrice;
+	currency: ExchangeLatest;
+	kline: BinanceKline;
+	address: string;
+}
+
+export default function Home({ ethermine, binance, currency, kline, address }: Props): JSX.Element {
+	return (
+		<div className={styles.container}>
+			<div className={styles.data}>
+				<Unpaid 
+					initialPool={ethermine}
+					initialUsd={binance.price}
+					foreign={currency.rates.SEK}
+					address={address} 
+				/>
+				<Wallet 
+					initialUsd={binance.price}
+					foreign={currency.rates.SEK}
+					address={address}
+				/>
+				<AverageHashrate 
+					initialPool={ethermine}
+					address={address}
+				/>
+				<EthPrice foreign={currency.rates.SEK} initialUsd={binance.price} />
+				<TotalHoldings 
+					initialPool={ethermine}
+					initialUsd={binance.price}
+					foreign={currency.rates.SEK}
+					address={address}
+				/>
+			</div>
+
+			<div className={styles.charts}>
+				<BinanceCandlestick kline={kline} />
+				<HashrateLineChart initialPool={ethermine} address={address} />
+			</div>
+		</div>
+	)
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const address = ctx.query.address;
+	const [res1, res2, res3, res4] = await Promise.all([
+		fetch(`https://api.ethermine.org/miner/${address}/currentStats`),
+		fetch("https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT"),
+		fetch("https://api.exchangerate.host/latest?base=usd&symbols=SEK"),
+		fetch("https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1m&limit=50")
+	]);
+	const [ethermine, binance, currency, kline] = await Promise.all([
+		res1.json(), 
+		res2.json(), 
+		res3.json(),
+		res4.json()
+	])
+	return {
+		props: {
+			ethermine,
+			binance,
+			currency,
+			kline,
+			address
+		}
+	};
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,35 @@
-import { GetServerSideProps } from "next"
+import React, { ChangeEvent, FormEvent, ReactElement, useState } from 'react';
+import { useRouter } from "next/router";
 
-import EthPrice from "../components/Data/EthPrice";
-import Unpaid from "../components/Data/Unpaid";
-import Wallet from "../components/Data/Wallet";
-import AverageHashrate from "../components/Data/AverageHashrate";
-import TotalHoldings from "../components/Data/TotalHoldings";
-import BinanceCandlestick from "../components/Data/BinanceCandlestick";
-import HashrateLineChart from "../components/Data/HashrateLineChart";
+import styles from "../styles/Home.module.css";
 
-import styles from '../styles/Home.module.css'
-import { MinerCurrentStats } from "../types/ethermine";
-import { BinanceAvgPrice, BinanceKline } from "../types/binance";
-import { ExchangeLatest } from "../types/exchangerate";
+export default function Home(): ReactElement {
+	const [address, setAddress] = useState("");
+	const router = useRouter();
 
-interface Props{
-	ethermine: { data: MinerCurrentStats };
-	binance: BinanceAvgPrice;
-	currency: ExchangeLatest;
-	kline: BinanceKline;
-	address: string;
-}
+	function onChange(e: ChangeEvent<HTMLInputElement>){
+		const value = e.currentTarget.value;
+		setAddress(value);
+	}
 
-export default function Home({ ethermine, binance, currency, kline, address }: Props): JSX.Element {
+	function handleSubmit(e: FormEvent){
+		e.preventDefault();
+
+		router.push("/" + address);
+	}
+
 	return (
 		<div className={styles.container}>
-			<div className={styles.data}>
-				<Unpaid 
-					initialPool={ethermine}
-					initialUsd={binance.price}
-					foreign={currency.rates.SEK}
-					address={address} 
-				/>
-				<Wallet 
-					initialUsd={binance.price}
-					foreign={currency.rates.SEK}
-					address={address}
-				/>
-				<AverageHashrate 
-					initialPool={ethermine}
-					address={address}
-				/>
-				<EthPrice foreign={currency.rates.SEK} initialUsd={binance.price} />
-				<TotalHoldings 
-					initialPool={ethermine}
-					initialUsd={binance.price}
-					foreign={currency.rates.SEK}
-					address={address}
-				/>
+			<div className={styles.copy}>
+				<h1>Know what your ETH is worth, <br/> in real-time</h1>
+				<p>Monitor the value of your unpaid ETH, as well as your wallet balance. See the value in both USD and your local currency (currently only SEK).</p>
 			</div>
-
-			<div className={styles.charts}>
-				<BinanceCandlestick kline={kline} />
-				<HashrateLineChart initialPool={ethermine} address={address} />
-			</div>
+			<form action="address" onSubmit={handleSubmit} className={styles.form}>
+				<label htmlFor="address">Enter miner address</label>
+				<input id="address" name="address" value={address} onChange={onChange} placeholder="0x35bB09d9180b520B5b8f5e5B86Cc595DDA4cA0C7" />
+				<button>Let's go!</button>
+			</form>
 		</div>
+		
 	)
-}
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const address = ctx.query.address;
-	const [res1, res2, res3, res4] = await Promise.all([
-		fetch(`https://api.ethermine.org/miner/${address}/currentStats`),
-		fetch("https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT"),
-		fetch("https://api.exchangerate.host/latest?base=usd&symbols=SEK"),
-		fetch("https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1m&limit=50")
-	]);
-	const [ethermine, binance, currency, kline] = await Promise.all([
-		res1.json(), 
-		res2.json(), 
-		res3.json(),
-		res4.json()
-	])
-	return {
-		props: {
-			ethermine,
-			binance,
-			currency,
-			kline,
-			address
-		}
-	};
 }
