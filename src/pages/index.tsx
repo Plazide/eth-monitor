1ed567a862b5237f56ed4230f5766fce3c26ed24
Carl Hallén Jansson <callehj@hotmail.se>
2021-06-12 10:22:51 +0200

    Replace address input with 'connect with wallet' button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,17 @@
-import React, { ChangeEvent, FormEvent, ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { useRouter } from "next/router";
 
 import styles from "../styles/Home.module.css";
+import { connectToWallet } from '../util/eth';
 
 export default function Home(): ReactElement {
-	const [address, setAddress] = useState("");
 	const router = useRouter();
 
-	function onChange(e: ChangeEvent<HTMLInputElement>){
-		const value = e.currentTarget.value;
-		setAddress(value);
-	}
+	async function handleClick(){
+		const accounts = await connectToWallet();
+		const account = accounts[0];
 
-	function handleSubmit(e: FormEvent){
-		e.preventDefault();
-
-		router.push("/" + address);
+		router.push("/" + account);
 	}
 
 	return (
@@ -24,11 +20,8 @@ export default function Home(): ReactElement {
 				<h1>Know what your ETH is worth, <br/> in real-time</h1>
 				<p>Monitor the value of your unpaid ETH, as well as your wallet balance. See the value in both USD and your local currency (currently only SEK).</p>
 			</div>
-			<form action="address" onSubmit={handleSubmit} className={styles.form}>
-				<label htmlFor="address">Enter miner address</label>
-				<input id="address" name="address" value={address} onChange={onChange} placeholder="0x35bB09d9180b520B5b8f5e5B86Cc595DDA4cA0C7" />
-				<button>Let's go!</button>
-			</form>
+
+			<button onClick={handleClick}>Connect with Wallet</button>
 		</div>
 		
 	)
--- src/util/eth.ts
+++ src/util/eth.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/6f44f377eea84bd083a439f89c643178"));
+// const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/6f44f377eea84bd083a439f89c643178"));
+const web3 = new Web3(Web3.givenProvider);
 
 export const ether = 1_000_000_000_000_000_000;
 
@@ -22,4 +23,18 @@ export function useEthBalance(address: string): number{
 	}, []);
 
 	return balance;
+}
+
+export async function connectToWallet(): Promise<string[]>{
+	const extended = await web3.eth.personal.extend({
+		methods: [
+			{
+				call: "eth_requestAccounts",
+				name: "requestAccounts"
+			}
+		]
+	});
+	
+	const accounts = await extended.requestAccounts();
+	return accounts;
 }
